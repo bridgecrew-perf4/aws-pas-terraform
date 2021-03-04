c179a8e64a856767b60fcfabe43378a6abb4a628
Ernie Billing <ebilling@pivotal.io>
2021-03-04 13:56:45 -0800

    confugure srt helper

---
+++ scripts/configure-srt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PROJECT_DIR="${SCRIPT_DIR}/.."
+
+$SCRIPT_DIR/configure-product $1 cf $2
